a19eeb0a05e7039072691d2a2cb6d13337c19e02
Robert Schönherr <admin@rbs90.de>
2021-11-18 14:52:59 +0100

    changed to template variant

--- script.js
+++ script.js
@@ -33,36 +33,64 @@ function injectTrackingOverviewinPage(tracking_infos) {
   if (old_version)
     old_version.remove();
 
-  var node = document.createElement("div");
-  node.setAttribute("id", "order_tracker");
-  var html = "<h4>Tracking overview</h4>";
-  for (var i = 0; i < tracking_infos.length; i++) {
-    console.log(tracking_infos[i].order_id);
-    html += "<div class='row package'>";
-    let tracking = tracking_infos[i].tracking[0];
-    mailNo = tracking.consoTagSecondMailNo.length > 0 ? tracking.consoTagSecondMailNo : tracking.mailNo;
-    html += "<div class='col-md-12 mailNo'><a href='" + tracking_infos[i].cainiaoUrl + "'>" + mailNo + "</a></div>";
-    html += "<div class='col-md-12'>" + tracking.keyDesc + "</div>";
-    if (tracking.traceList) {
-      html += "<div class='col-md-24'>" + tracking.traceList[0].desc.trim() + "</div>";
-      html += "<div class='col-md-12'>" + tracking.traceList[0].eventTimeStr + "</div>";
+  // group by mailNo (combined packages)
+  const trackingNumberMap = new Map();
+  tracking_infos.forEach(info => {
+    const mailNo = info.tracking[0].consoTagSecondMailNo.length > 0 ? info.tracking[0].consoTagSecondMailNo : info.tracking[0].mailNo;
+    const collection = trackingNumberMap.get(mailNo);
+    if (!collection) {
+      trackingNumberMap.set(mailNo, [info]);
     } else {
-      html += "<div class='col-md-24'></div>";
-      html += "<div class='col-md-12'></div>";
+      collection.push(info);
     }
-    html += "<div class='col-xs-60 order_imgs'>";
-    let img_search = document.evaluate("//td[@class='order-info']/p[@class='first-row']/span[@class='info-body' and text()='" + tracking_infos[i].order_id + "']/parent::p/parent::td/parent::tr/parent::tbody//div[@class='product-left']//img", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
-    for (var j = 0; j < img_search.snapshotLength; j++) {
-      var img_node = img_search.snapshotItem(j);
-      html += "<div class='col-md-4'>" + img_node.outerHTML + "</div>";
-    }
-    html += "</div>";
-    html += "</div>";
+  });
 
-  }
-  node.innerHTML = html;
+  var html = `<div id="order_tracker"><h4>Tracking overview</h4>`;
+
+  trackingNumberMap.forEach((tracking_infos, mailNo) => {
+    let tracking = tracking_infos[0].tracking[0];
+    const tracking_header = {
+      mailNo,
+      url: tracking_infos[0].cainiaoUrl,
+      status_coarse: tracking.keyDesc,
+      status_fine: "",
+      last_update: ""
+    }
+    if (tracking.traceList) {
+      tracking_header.status_fine = tracking.traceList[0].desc;
+      tracking_header.last_update = tracking.traceList[0].eventTimeStr;
+    }
+
+    if (tracking_infos.length > 1) {
+      tracking_header.mailNo += " (Combined)";
+    }
+
+    html += `<div class='row package'>
+    <div class='col-md-12 mailNo'><a href="${tracking_header.url}">${tracking_header.mailNo}</a></div>
+    <div class='col-md-12'>${tracking_header.status_coarse}</div>
+    <div class='col-md-24'>${tracking_header.status_fine}</div>
+    <div class='col-md-12'>${tracking_header.last_update}</div>
+    `
+    tracking_infos.forEach((tracking) => {
+      html += `<div class='col-xs-60 order_imgs'>`;
+      let img_search = document.evaluate("//td[@class='order-info']/p[@class='first-row']/span[@class='info-body' and text()='" + tracking.order_id + "']/parent::p/parent::td/parent::tr/parent::tbody//div[@class='product-left']//img", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+      for (var j = 0; j < img_search.snapshotLength; j++) {
+        var img_node = img_search.snapshotItem(j);
+        html += `<div class='col-md-4'><img src="${img_node.getAttribute("src")}"></img></div>`;
+      }
+      html += `</div>`;
+    })
+
+    html += `</div>`;
+
+  });
+  html += "</div>"
+
+  const parser = new DOMParser()
+  const parsed = parser.parseFromString(html, `text/html`)
+
   orderTableNode = document.getElementById("buyer-ordertable");
-  orderTableNode.parentNode.insertBefore(node, orderTableNode.nextSibling);
+  orderTableNode.parentNode.insertBefore(parsed.getElementById("order_tracker"), orderTableNode.nextSibling);
 }
 
 var result = document.evaluate(
